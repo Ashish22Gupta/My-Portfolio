37f75377a0fc1e198ec8e61fe455fcca8456c8c7
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2025-01-12 14:08:48 +0530

    Update Theme.jsx

--- src/Component/Theme.jsx
+++ src/Component/Theme.jsx
@@ -13,7 +13,7 @@ const Theme = ({ theme, setTheme,bg,textColor }) => {
     <div className={`${bg} ${textColor}shadow-lg shadow-white-500/50 rounded-sm mt-3 ml-3 flex items-center h-20 text-center max-xl:mr-3 max-xl:h-fit`}>
       <div className="ml-3">
         <h1 className="text-xl font-medium max-xl:text-6xl">Theme</h1>
-        <p className="max-xl:text-4xl">{theme}</p> {/* Display selected theme */}
+        <p className="max-xl:text-4xl">{theme}</p>
       </div>
       <div className="flex items-center ml-auto mr-3">
         <Sliders className="ml-9 max-xl:ml-10 max-xl:h-12 max-xl:w-12" color={theme === 'Dark' ? 'white' : 'black'} size={20} />
@@ -30,20 +30,18 @@ const Theme = ({ theme, setTheme,bg,textColor }) => {
             className="absolute z-10 mt-2 left-0 right-0 mx-auto w-auto max-xl:w-auto origin-top divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in">
             <div className="py-1">
               <MenuItem>
-                <a
-                  href="#"
+                <span
                   onClick={() => handleThemeChange('Dark')}
                   className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 max-xl:text-4xl">
                   Dark
-                </a>
+                </span>
               </MenuItem>
               <MenuItem>
-                <a
-                  href="#"
+                <span
                   onClick={() => handleThemeChange('Light')}
                   className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 max-xl:text-4xl">
                   Light
-                </a>
+                </span>
               </MenuItem>
             </div>
           </MenuItems>
