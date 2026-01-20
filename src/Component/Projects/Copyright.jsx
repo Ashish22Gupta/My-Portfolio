ab62a68e4ad90498ef16543496cea78fd05632ef
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2026-01-20 22:17:44 +0530

    Update Copyright.jsx

--- src/Component/Projects/Copyright.jsx
+++ src/Component/Projects/Copyright.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-const Copyright = () => {
+
+const Copyright = ({currentTheme}) => {
   return (
-    <div className='w-full flex flex-row justify-center text-lg text-slate-400 max-xl:pb-12 max-xl:text-3xl'>Copyright © 2024 Ashish Gupta - All Rights are Reserved</div>
+    <div className={`w-full flex flex-row justify-center text-lg ${currentTheme.main} max-xl:pb-12 max-xl:text-3xl`}>
+      Copyright © 2026 Ashish Gupta - All Rights are Reserved
+    </div>
   )
 }
 
